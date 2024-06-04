a1be3196e21c3244734c92e6d58f411a0ac7ca89
Adrian Kulmburg <adrian.kulmburg@tum.de>
2024-06-04 20:11:29 +0200

    Cosmetic changes

--- p_equals_1/draw_plot.m
+++ p_equals_1/draw_plot.m
@@ -1,30 +1,32 @@
-n_range = 1:6;
-ell_range = 2:20;
+n_range = 1:9;
+m_range = 2:20;
 
 clf;
 hold on
 
-for ell = ell_range
+for m = m_range
     worst_case = 1;
     for n = n_range
         try
-            filename = ['data/',num2str(n),'_',num2str(ell),'_',num2str(ell),'.mat'];
+            filename = ['data/',num2str(n),'_',num2str(m),'_',num2str(m),'.mat'];
             load(filename)
             worst_case = max(worst_case, 1/fval);
         catch
             continue
         end
     end
-    data_points = plot(ell,worst_case,'ok');
+    data_points = plot(m,worst_case,'ok');
 end
 
 x = linspace(2, 20, 1000);
 theory = plot(x, sqrt(x), 'k');
 
 title("Worst-case approximation ratio for $q=1$, $p=1$", 'Interpreter', 'latex')
-xlabel("$\ell$", 'Interpreter', 'latex')
+xlabel("$m$", 'Interpreter', 'latex')
 ylabel("Approximation Ratio", 'Interpreter', 'latex')
 
-lgd = legend([data_points theory], {'$\rho_{\max}$', '$C_1\cdot\sqrt{\ell}$'}, 'Interpreter', 'latex', 'Location', 'northwest');
+ylim([1 5])
+
+lgd = legend([data_points theory], {'Linear Relaxation', '$\sqrt{m}$'}, 'Interpreter', 'latex', 'Location', 'northwest');
 
 %matlab2tikz('p_equals_1.tex')
--- p_variable/draw_plot.m
+++ p_variable/draw_plot.m
@@ -1,38 +1,39 @@
-p = 2;
+p = 1.5;
+%p = 2;
 
 
 n_range = 1:6;
-ell_range = 2:2:12;
+m_range = 2:2:12;
 
 clf;
 hold on
 
-for ell = ell_range
+for m = m_range
     worst_case = 0;
     for n = n_range
         try
-            filename = ['data/',num2str(p),'_',num2str(n),'_',num2str(ell),'_',num2str(ell),'_linear.mat'];
+            filename = ['data/',num2str(p),'_',num2str(n),'_',num2str(m),'_',num2str(m),'_linear.mat'];
             load(filename)
             worst_case = max(worst_case, 1/fval);
         catch
             continue
         end
     end
-    data_points_linear = plot(ell,worst_case,'ok');
+    data_points_linear = plot(m,worst_case,'ok');
 end
 
-for ell = ell_range
+for m = m_range
     worst_case = 0;
     for n = n_range
         try
-            filename = ['data/',num2str(p),'_',num2str(n),'_',num2str(ell),'_',num2str(ell),'_semidefinite.mat'];
+            filename = ['data/',num2str(p),'_',num2str(n),'_',num2str(m),'_',num2str(m),'_semidefinite.mat'];
             load(filename)
             worst_case = max(worst_case, 1/fval);
         catch
             continue
         end
     end
-    data_points_semidefinite = plot(ell,worst_case,'xk');
+    data_points_semidefinite = plot(m,worst_case,'xk');
 end
 
 gamma_r = @(r) (2^(r/2)/sqrt(pi) * gamma((r+1)/2))^(1/r);
@@ -47,10 +48,10 @@ end
 
 
 title("Worst-case approximation ratio for $q=1$, $p="+num2str(p)+"$", 'Interpreter', 'latex')
-xlabel("$m$ (in this case, $m=l$, $2n = m$)", 'Interpreter', 'latex')
+xlabel("$m$ (in this case, $m=l$, $m = 2n$)", 'Interpreter', 'latex')
 ylabel("Approximation Ratio", 'Interpreter', 'latex')
 
-%ylim([1 5])
+ylim([1 5])
 
 if p>= 2
     lgd = legend([data_points_linear data_points_semidefinite theory_linear theory_semidefinite], {'Linear Relaxation', 'Semidefinite Relaxation', '$\sqrt{m}\cdot\gamma_p/\gamma_1$', '$\gamma_p/\gamma_1$'}, 'Interpreter', 'latex', 'Location', 'northwest');
